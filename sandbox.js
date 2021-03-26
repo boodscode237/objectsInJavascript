ab65cf38f69ab118833767e471ee500392b5631e
boodscode237 <boodscode@gmail.com>
2021-03-26 08:43:38 +0500

    Math object

--- sandbox.js
+++ sandbox.js
@@ -17,10 +17,15 @@ let user = {
     }
 }
 
-// console.log(`${user.name}, \n ${user.blog[1]}`)
-// user['name'] = 'Johnny'
-// console.log(` 1 ${user.name}, \n ${user.blog[1]}`)
-// user.name = "Johnn"
-// console.log(` 3 ${user.name}, \n ${user.blog[1]}`)
 
-user.logBlogs()
+// math object
+
+let area = 7.7;
+console.log(Math.round(area)); // rounds the number to the nearest int
+console.log(Math.floor(area)); //brings the number to the lowest int
+// console.log(Math.ciel(area)); // brings the number to the upper int
+console.log(Math.trunc(area)); //removes the decimal part
+
+const random = Math.random() * 100; //give a random number between 0.0 and 0.9
+
+console.log(random);
